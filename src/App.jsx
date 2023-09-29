793ed2df728dbabb841c259de9dc97727f7974e8
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2023-09-29 13:19:53 +0200

    recomendaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
-import React, { useEffect, useState } from "react"
-import './App.css'
-import Card from "./components/carta/Carta"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/carta/Carta";
 //import axios from "axios"
 
-
-
 function App() {
+    const [pokemon, setPokemon] = useState(); // mejor inicializar el estado a null que dejarlo vacio.
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/22");
+    // Si utilizar la url como el estado para no repetir el codigo y tener la url centralizada porque no creas una variable
+    // fuera del compoennte con la BASE_URL y despues le añades el numero,
+    // tambien otra opcion es solo controlando el estado del random
 
+    function getPokemon() {
+        const random = Math.floor(Math.random() * 1000);
+        setUrl(`https://pokeapi.co/api/v2/pokemon/${random}`);
+    }
+    useEffect(() => {
+        /* Podrias añadir como tarea extra añadir un estaso de loading para que se muestre feedback enlos campos 
+      y en la imagen como que esta cargando la imagen. */
+        const getPokemonData = async () => {
+            const reponse = await fetch(url); // te comiste una s, en una entrega mejor no arrastrar errores de estos si se puede.
+            const data = await reponse.json();
+            setPokemon(data);
+        };
 
-  const [pokemon, setPokemon] = useState()
-  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/22")
-
-  function getPokemon(){
-    const random = Math.floor (Math.random() * 1000)
-    setUrl(`https://pokeapi.co/api/v2/pokemon/${random}`)
-  }
-  useEffect(() => {
-
-      const getPokemonData = async() => {
-        const reponse = await fetch(url);
-        const data = await reponse.json()
-        setPokemon(data)
-      }
-
-     /**const getPokemonData = async () => {
+        /**const getPokemonData = async () => {
     const url = ("https://pokeapi.co/api/v2/pokemon/22");
     const result = await axios.get(url);
     const data = await result.json()
     setPokemon(result.data)
     }*/
-      
-    getPokemonData()
-  }, [url]);
-  
 
-  return (
-    <>
-      {pokemon && <Card pokemon = {pokemon}/>}
-      <button className="bg-blue-200 mt-3 p-4 text-blue-950 drop-shadow-md font-bold hover:scale-110" 
-        onClick={getPokemon}
-        >Cargar Nuevo Pokemon
-      </button>
-    </>
-  )
+        getPokemonData();
+    }, [url]);
+
+    return (
+        <>
+            {pokemon && <Card pokemon={pokemon} />}
+            <button
+                className="bg-blue-200 mt-3 p-4 text-blue-950 drop-shadow-md font-bold hover:scale-110"
+                onClick={getPokemon}
+            >
+                Cargar Nuevo Pokemon
+            </button>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/carta/Carta.jsx
+++ src/components/carta/Carta.jsx
@@ -1,42 +1,68 @@
+import React from "react"; // con vite no es necessario importar react ya que lo hace vite por detras
 
-import React from "react"
-
-export default function Card({pokemon}) {
-
-
-    function getImage(){
+export default function Card({ pokemon }) {
+    function getImage() {
         const src = pokemon.sprites[`front_default`];
-        return src
-     }
+        return src; // Un poco innecesario crear la constante, piendolo devolverlo en la misma linea
+    }
 
-        return (
-        <div  className=" w-80 h-100 bg-white rounded-xl drop-shadow-xl ">
+    return (
+        <div className=" w-80 h-100 bg-white rounded-xl drop-shadow-xl ">
             <div className="flex flex-col items-center rounded-xl ">
+                {/* para colocar el fondo bien, tienes que ponerlo absoluto en relacion al div padre 
+                y asi podras superponer la imagen del pokemon */}
                 <div className="bg-card  w-full rounded-t-xl h-22">
-                <img src= {getImage()} className="-z-4 ml-20 mt-10 w-40 h-50 rounded-full hover:shadow-lg bg-white hover:scale-110 " alt={`Pokemon`}
-                />
+                    <img
+                        src={getImage()}
+                        /* aqui sobran muchismas clases, fijate que h-50 es una clase de tailwind que ni exite, deberian
+                        ser width y height iguales o solo poner el height valdria el margin-left es una manera muy pobre de centrar algo, 
+                        si decides hacer mas grande la card ya no estaria centrado, prueba aprovechando el items-center delpadre y sacando
+                        el img del div del bg.
+                        Tambien es recomendable si poner un efecto en hover, darle una clases de trasicion tipo transition-all para hacerlo mas 
+                        agradable para el usuario. */
+                        className="-z-4 ml-20 mt-10 w-40 h-50 rounded-full hover:shadow-lg bg-white hover:scale-110 "
+                        alt={`Pokemon`}
+                    />
+                </div>
+                <div className="pb-10">
+                    {/* creo que se quires separa algo es mejor utilizar margin ya que padding deja el div asimetrico. */}
+                    <p className="text-gray-800 first-letter:uppercase text-lg font-bold mt-4">
+                        {pokemon.name}
+                        <span className="text-gray-600 font-thin ">
+                            {" "}
+                            {pokemon.stats[0].base_stat} hp
+                        </span>
+                    </p>
+                    <p className="text-gray-600 mt-3">
+                        {pokemon.base_experience} exp
+                    </p>
+                </div>
             </div>
-            <div className="pb-10">
-                <p className="text-gray-800 first-letter:uppercase text-lg font-bold mt-4" >{pokemon.name}
-                <span className="text-gray-600 font-thin "> {pokemon.stats[0].base_stat} hp</span></p>
-                <p className="text-gray-600 mt-3">{pokemon.base_experience} exp</p>
-            </div>
-            
-        </div>
 
-        <div className="grid grid-cols-3 gap-3 p-3">
-            <div className="relative ">
-                <p className="text-black font-bold">{pokemon.stats[1].base_stat}K</p>                    
-                <p className=" text-black font-thin text-sm">Ataque</p>
-            </div>
-            <div className="relative ">
-                <p className="text-black font-bold">{pokemon.stats[3].base_stat}K</p>
-                <p className=" text-black font-thin text-sm">Ataque Especial</p>
-            </div>                <div className=" relative ">
-                <p className="text-black font-bold">{pokemon.stats[2].base_stat}K</p>
-                <p className=" text-black font-thin text-sm">Defensa</p>
+            <div className="grid grid-cols-3 gap-3 p-3">
+                {/* Por que poner relative? */}
+                <div className="relative ">
+                    <p className="text-black font-bold">
+                        {pokemon.stats[1].base_stat}K
+                    </p>
+                    <p className=" text-black font-thin text-sm">Ataque</p>
+                </div>
+                {/* Por que poner relative? */}
+                <div className="relative ">
+                    <p className="text-black font-bold">
+                        {pokemon.stats[3].base_stat}K
+                    </p>
+                    <p className=" text-black font-thin text-sm">
+                        Ataque Especial
+                    </p>
+                </div>{" "}
+                <div className=" relative ">
+                    <p className="text-black font-bold">
+                        {pokemon.stats[2].base_stat}K
+                    </p>
+                    <p className=" text-black font-thin text-sm">Defensa</p>
+                </div>
             </div>
         </div>
-        </div>
-      )
+    );
 }
